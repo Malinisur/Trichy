5c71f3a72b769085abecc8e75ff6937797e740db
malinisuresh <malinisuresh6@gmail.com>
2022-12-04 16:14:32 +0000

    This is a new commit for what I originally planned to be amended

--- welcome.java
+++ welcome.java
@@ -2,5 +2,6 @@ public class welcome {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
 System.out.println ("Welcome to trichy");
+System.out.printly ("Trichy also called as Rockfort")
   }
 }
