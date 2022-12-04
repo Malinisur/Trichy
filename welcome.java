e3c2ac5fbdae52846fdadaa81f7f4174e14ea0d4
malinisuresh <malinisuresh6@gmail.com>
2022-12-04 16:38:39 +0000

    Added new line to the repo

--- welcome.java
+++ welcome.java
@@ -2,6 +2,7 @@ public class welcome {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
 System.out.println ("Welcome to trichy");
-System.out.printly ("Trichy also called as Rockfort")
+System.out.println ("Trichy also called as Rockfort")
+System.out.println ("Have a nice day")
   }
 }
